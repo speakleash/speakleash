966bae05b8ea84085d4246fba2440d178e09a201
Sebastian Kondracki <sebastian.kondracki@deviniti.com>
2022-11-13 20:04:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="speakleash",
+    version="0.0.1",
+    author="SpeakLeash",
+    author_email="dude@speakleash.org",
+    description="SpeakLeash agnostic dataset for Polish",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/speakleash/speakleash",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'tqdm',
+        'lm_dataformat'
+    ]
+)

87bee6d74e4e4b49bc4c46bc2f5503655cafbb4e
Sebastian Kondracki <sebastian.kondracki@deviniti.com>
2023-07-04 20:33:29 +0200

    Quality metrics and category manager

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="speakleash",
-    version="0.1.1",
+    version="0.2.0",
     author="SpeakLeash Team",
     author_email="team@speakleash.org",
     description="SpeakLeash agnostic dataset for Polish",
--- speakleash/__init__.py
+++ speakleash/__init__.py
@@ -6,6 +6,7 @@ from lm_dataformat import Reader
 import hashlib
 from datetime import datetime
 import glob
+import tempfile
 
 class StructureDownloader(object):
 
@@ -62,6 +63,106 @@ class StructureDownloader(object):
             pass
         
         return data
+
+
+class CategoryManager(object):
+
+    def __init__(self):
+
+        self.temp_dir = os.path.join(tempfile.gettempdir(), "speakleash")
+
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir, exist_ok=True)
+
+        self.categories_pl = self.__categories("pl")
+        self.categories_en = self.__categories("en")
+        
+    def __categories(self, lang = "pl"):
+
+        url = ""
+        categories = []
+
+        if lang == "pl":
+            url = "https://speakleash.space/datasets_text/categories_pl.txt"
+
+        if lang == "en":
+            url = "https://speakleash.space/datasets_text/categories_en.txt"
+
+        if url:
+
+            if os.path.exists(os.path.join(self.temp_dir, lang + "_categories.txt")):
+                with open(os.path.join(self.temp_dir, lang + "_categories.txt"), 'r') as f:
+                    categories = f.readlines()
+                    categories = [line.strip() for line in categories]
+                return categories
+
+            try:
+                data = ""
+                r = requests.get(url)
+                r.encoding = 'utf-8' 
+                if r.ok:
+                    data = r.text
+            except:
+                pass
+
+            categories = data.split("\n")
+        
+            with open(os.path.join(self.temp_dir, lang + "_categories.txt"), encoding="utf-8", mode = 'w') as f:
+                for c in categories:
+                    f.write(c + "\n")
+
+        return categories
+    
+    def categories(self, lang = "pl"):
+
+        if lang == "pl":
+            return self.categories_pl
+        if lang == "en":
+            return self.categories_en
+        
+        return []
+    
+    def __get_pl_category(self, name, lang ):
+
+        index = None
+
+        if lang == "en":
+            try:
+                index = self.categories_en.index(name)
+            except:
+                pass
+
+        if index is not None:
+            return self.categories_pl[index]
+
+        return None
+    
+    def check_category(self, meta, categories, cf, lang = "pl"):
+
+        if not meta:
+            return False
+        
+        if len(categories) == 0:
+            return False
+        
+        for category in categories:
+            
+            category_pl = None
+            if lang != "pl":
+                category_pl = self.__get_pl_category(category, lang)
+            else:
+                category_pl = category
+
+            if category_pl:
+                meta_categories = meta.get("category", {})
+                print(meta_categories)
+                for meta_category in meta_categories:
+                    if meta_category.upper() == category_pl.upper():
+                        if meta_categories[meta_category] >= cf:
+                            return True 
+
+        return False
+
 
 class Speakleash(object):
 
@@ -133,6 +234,31 @@ class SpeakleashDataset(object):
     def characters(self):
         s = self.manifest.get("stats",{}).get("characters",0)
         return s
+    
+
+    @property
+    def quality_metrics(self):
+        h = self.manifest.get("stats",{}).get("quality",{}).get("HIGH",0)
+        l = self.manifest.get("stats",{}).get("quality",{}).get("LOW",0)
+        m = self.manifest.get("stats",{}).get("quality",{}).get("MEDIUM",0)
+        return not (h==0 and m==0 and l==0)
+    
+    @property
+    def categorization(self):
+        category = self.manifest.get("category=95%",{})
+        for c in category:
+            if category[c] > 0:
+                return True
+        return False
+     
+    @property
+    def categories(self):
+        category = self.manifest.get("category=95%",{})
+        return category
+
+    @property
+    def quality(self):
+        return self.manifest.get("stats",{}).get("quality",{})
 
     @property
     def documents(self):

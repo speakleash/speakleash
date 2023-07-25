eea519515ea7cf7e50c84c3381c49b908c88c446
IgorTest19 <quattroporte54@gmail.com>
2023-07-25 18:16:34 +0200

    structure downloader refactoring and core module changes

--- speakleash/core/core.py
+++ speakleash/core/core.py
@@ -1,27 +1,28 @@
 """
 Speakleash Module
 
-This module provides the Speakleash class, the main entity object responsible for managing and accessing datasets.
+This module provides the Speakleash class, the main entity object
+responsible for managing and accessing datasets.
 
 Classes:
 - Speakleash: Represents the Speakleash class.
 
 Dependencies:
+- typing: Provides the List and Optional types for type hinting.
 - speakleash.dataset: Provides the SpeakleashDataset class for dataset handling.
 - speakleash.downloader: Provides the StructureDownloader class for downloading dataset structures.
 - speakleash.config_loader: Provides the ConfigLoader class for loading configuration.
-
 """
+from typing import List, Optional
 
 from speakleash.dataset import SpeakleashDataset
 from speakleash.downloader import StructureDownloader
-
 from speakleash.config_loader import ConfigLoader
-
 
 class Speakleash:
     """
-    Represents the Speakleash class, main entity object, responsible for managing and accessing datasets.
+    Represents the Speakleash class, main entity object,
+    responsible for managing and accessing datasets.
     """
 
     def __init__(self, replicate_dir: str, lang: str = "pl"):
@@ -29,36 +30,32 @@ class Speakleash:
         Initialize an instance of Speakleash class.
 
         :param replicate_dir: The directory to replicate the datasets.
-        :type replicate_dir: str
         :param lang: The language for the Speakleash datasets (default is 'pl').
-        :type lang:
         """
         self.replicate_dir = replicate_dir
         self.structure_file = self.get_structure_file(lang)
         self.url = self.get_url(lang)
         self.datasets = self.populate_datasets()
 
-    def get_structure_file(self, lang: str) -> str:
+    @staticmethod
+    def get_structure_file(lang: str) -> str:
         """
         Retrieves the structure file based on the specified language.
 
         :param lang: The language code ('pl' or 'hr').
-        :type lang: str
         :return: The name of the structure file corresponding to the language.
-        :rtype: str
         """
         if lang == 'hr':
             return "speakleash_hr.json"
         return "speakleash.json"
 
-    def get_url(self, lang: str) -> str:
+    @staticmethod
+    def get_url(lang: str) -> str:
         """
         Retrieves the URL based on the provided language.
 
         :param lang: The language code ('pl' or 'hr').
-        :type lang: str
         :return: The URL corresponding to the provided language.
-        :rtype: str
         """
         urls_config = ConfigLoader.load_config()
 
@@ -66,11 +63,11 @@ class Speakleash:
             return urls_config["url_datasets_text_hr"]
         return urls_config["url_datasets_text_pl"]
 
-    def populate_datasets(self) -> list:
+    def populate_datasets(self) -> Optional[List[SpeakleashDataset]]:
         """
         Populates the datasets list by fetching dataset names.
 
-        :return: None
+        :return: A list of SpeakleashDataset instances, or None if no names are fetched.
         """
         names = StructureDownloader(self.replicate_dir).get_structure(
             self.url + self.structure_file)
@@ -78,17 +75,18 @@ class Speakleash:
             return [SpeakleashDataset(item["name"], self.url,
                                       self.replicate_dir) for item in names if
                     "name" in item]
+        return None
 
-    def get(self, name: str) -> SpeakleashDataset:
+    def get(self, name: str) -> Optional[SpeakleashDataset]:
         """
         Retrieves a dataset by its provided name.
 
         :param name: The name of the dataset.
-        :type name: str
-        :return: The SpeakleashDataset instance corresponding to the given name, or None if not found.
-        :rtype: SpeakleashDataset
+        :return: The SpeakleashDataset instance corresponding to the given name,
+        or None if not found.
         """
-        for dataset in self.datasets:
-            if dataset.name == name:
-                return dataset
+        if self.datasets:
+            for dataset in self.datasets:
+                if dataset.name == name:
+                    return dataset
         return None
--- speakleash/downloader/structure_downloader.py
+++ speakleash/downloader/structure_downloader.py
@@ -1,63 +1,144 @@
-import requests
+"""
+StructureDownloader Module
+
+This module provides the StructureDownloader class, an entity responsible for
+downloading and managing dataset structures.
+
+Classes:
+- StructureDownloader: Represents the StructureDownloader class.
+
+Dependencies:
+- hashlib: Provides the algorithm for generating a hash from a URL.
+- json: Provides methods for working with JSON data.
+- os: Provides a way of using operating system dependent functionality.
+- datetime: Provides classes for manipulating dates and times.
+- glob: Provides a function for creating lists of file paths matching a pathname pattern.
+- typing: Provides the Optional and Dict types for type hinting.
+- requests: Provides methods for sending HTTP requests.
+
+"""
+import hashlib
 import json
 import os
-import hashlib
 from datetime import datetime
 import glob
+from typing import Optional, Dict
+
+import requests
 
 
 class StructureDownloader:
+    """
+    Represents the StructureDownloader class, an entity responsible for
+    downloading and managing dataset structures.
+    """
 
-    def __init__(self, replicate_dir):
+    def __init__(self, replicate_dir: str):
+        """
+        Initialize an instance of the StructureDownloader class.
+
+        :param replicate_dir: The directory where the dataset structures are replicated.
+        """
         self.replicate_dir = replicate_dir
+        self.data: Optional[Dict] = None
 
-    def _remove_old_files(self, url):
+    @staticmethod
+    def generate_hash(url: str) -> str:
+        """
+        Generates a hash from a URL.
 
-        hash = hashlib.md5(url.encode('utf-8')).hexdigest()
-        filter = os.path.join(self.replicate_dir, hash + "-*.json")
-        files = glob.glob(filter)
-        for f in files:
+        :param url: The URL to be hashed.
+        :return: The hash of the URL.
+        """
+        return hashlib.md5(url.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def get_timestamp(hourly: bool = True) -> str:
+        """
+        Generates a timestamp.
+
+        :param hourly: If True, the timestamp will include the current hour.
+        Otherwise, it will only include the date. Defaults to True.
+        :return: The timestamp.
+        """
+        now = datetime.now()
+        if hourly:
+            return now.strftime("-%m_%d_%y_%H")
+        return now.strftime("-%m_%d_%y")
+
+    def _remove_old_files(self, url: str) -> None:
+        """
+        Removes old files that match the hash of the URL.
+
+        :param url: The URL for generating the hash.
+        """
+        url_hash = self.generate_hash(url)
+        absolute_file_path = os.path.join(self.replicate_dir, f"{url_hash}-*.json")
+        files = glob.glob(absolute_file_path)
+
+        for file in files:
             try:
-                os.remove(f)
+                os.remove(file)
             except:
                 pass
 
-        return
+    def get_data_from_file(self, file: str) -> None:
+        """
+        Loads data from a file.
 
-    def get_structure(self, url, hourly=True):
+        :param file: The file path.
+        """
+        try:
+            with open(file, 'r', encoding='utf-8') as data_file:
+                self.data = json.load(data_file)
+        except:
+            pass
 
-        now = datetime.now()
-        data = None
+    def get_data_from_url(self, url: str) -> None:
+        """
+        Downloads data from a URL.
 
-        if hourly:
-            ts = now.strftime("-%m_%d_%y_%H")
-        else:
-            ts = now.strftime("-%m_%d_%y")
+        :param url: The URL.
+        """
+        try:
+            response = requests.get(url)
+            if response.ok:
+                self.data = json.loads(response.text)
+        except:
+            pass
 
-        hash = hashlib.md5(url.encode('utf-8')).hexdigest()
-        file = os.path.join(self.replicate_dir, hash + ts + ".json")
+    @staticmethod
+    def write_data_to_file(data: Dict, file: str) -> None:
+        """
+        Writes data to a file.
+
+        :param data: The data to be written.
+        :param file: The path of the file.
+        """
+        try:
+            with open(file, 'w', encoding='utf-8') as data_file:
+                json.dump(data, data_file)
+        except:
+            pass
+
+    def get_structure(self, url: str, hourly: bool = True) -> Dict:
+        """
+        Retrieves the dataset structure. If the structure already exists in a file,
+        it is loaded from there. Otherwise, it is downloaded from the URL.
+
+        :param url: The URL for downloading the dataset structure.
+        :param hourly: If True, the timestamp will include the current hour.
+        Otherwise, it will only include the date. Defaults to True.
+        :return: The dataset structure.
+        """
+        timestamp = self.get_timestamp(hourly)
+        url_hash = self.generate_hash(url)
+        file = os.path.join(self.replicate_dir, f"{url_hash}{timestamp}.json")
 
         if os.path.exists(file):
-            try:
-                with open(file, 'r') as f:
-                    data = json.load(f)
-                return data
-            except:
-                pass
-
-        self._remove_old_files(url)
-
-        try:
-            r = requests.get(url)
-            if r.ok:
-                data = json.loads(r.text)
-        except:
-            pass
-
-        try:
-            with open(file, 'w') as f:
-                json.dump(data, f)
-        except:
-            pass
-
-        return data
+            self.get_data_from_file(file)
+        else:
+            self._remove_old_files(url)
+            self.get_data_from_url(url)
+            self.write_data_to_file(self.data, file)
+        return self.data
